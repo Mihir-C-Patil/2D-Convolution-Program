d3003614f9f898cf551b784692a8091a6d562c2e
Mihir Patil <mightymihir100@gmail.com>
2023-06-05 21:49:42 +1200

    Tidied up code, changed variable names, so they are descriptive.

--- convolution1.c
+++ convolution1.c
@@ -3,82 +3,77 @@
 #include<time.h>
 
 struct matrix {
-    int o_val;
-    int n_val;
+    int original_vals;
+    int new_vals;
 };
 typedef struct matrix Matrix;
 
 
 int main(int argc, char *argv[]) {
-    FILE *file1, *file2, *file3;
-    int i = 0;
+    FILE *data_file, *filter_file, *output_file;
+    int index = 0;
     int filter[5][5];
     Matrix** data;
-    int j, k, l, m;
-    int val;
-    int iter;
+    int index2;
+    int values;
+    int iterations;
 
     if (argc < 5) {
     	printf("Usage: ./convolution1 data1.txt filter1.txt temp111 1\n");
     	return 1;
     }
-    file1 = fopen(argv[1], "r");
-    file2 = fopen(argv[2], "r");
-    if ((file1 == NULL) || (file2 == NULL)) {
+    data_file = fopen(argv[1], "r");
+    filter_file = fopen(argv[2], "r");
+    if ((data_file == NULL) || (filter_file == NULL)) {
     	printf("Error: Files cannot open\n");
     	return 1;
     }
-    iter = atoi(argv[4]);
-    if (iter == 0) {
+    iterations = atoi(argv[4]);
+    if (iterations == 0) {
     	printf("Error: Invalid iteration\n");
     	return 1;
-    }  
-    file3 = fopen(argv[3], "w");
+    }
+    output_file = fopen(argv[3], "w");
     data = (Matrix**) malloc(sizeof(Matrix*)*1024);
-    for (i = 0; i < 1024; i++) {
-        data[i] = (Matrix*) malloc(sizeof(Matrix)*1024);
+    for (index = 0; index < 1024; index++) {
+        data[index] = (Matrix*) malloc(sizeof(Matrix) * 1024);
     }
 
-
-    i = 0;
+    index = 0;
     do {
-        j = 0;
+        index2 = 0;
         do {
-            if (j == 1023) {
-                fscanf(file1, "%d\n", &val);
-                data[i][j].o_val = val;
-//                printf("%d\n", data[i][j]);
+            if (index2 == 1023) {
+                fscanf(data_file, "%d\n", &values);
+                data[index][index2].original_vals = values;
             }
             else {
-                fscanf(file1, "%d,", &val);
-                data[i][j].o_val = val;
-//                printf("%d ", data[i][j]);
+                fscanf(data_file, "%d,", &values);
+                data[index][index2].original_vals = values;
             }
-            j = j + 1;
-        } while (j < 1024);
-        i = i + 1;
-    } while (i < 1024);
+            index2 = index2 + 1;
+        } while (index2 < 1024);
+        index = index + 1;
+    } while (index < 1024);
 
-    i = 0;
+    index = 0;
     do {
-        int j = 0;
+        index2 = 0;
         do {
-            if (j == 4) {
-                fscanf(file2, "%d\n", &val);
-                filter[i][j] = val;
-//                printf("%d\n", filter[i][j]);
+            if (index2 == 4) {
+                fscanf(filter_file, "%d\n", &values);
+                filter[index][index2] = values;
             }
             else {
-                fscanf(file2, "%d,", &val);
-                filter[i][j] = val;
-//               printf("%d ", filter[i][j]);
+                fscanf(filter_file, "%d,", &values);
+                filter[index][index2] = values;
             }
-            j = j + 1;
-        } while (j < 5);
-        i = i + 1;
-    } while (i < 5);
+            index2 = index2 + 1;
+        } while (index2 < 5);
+        index = index + 1;
+    } while (index < 5);
 
-    for (int iteration = 0; iteration < iter; ++iteration) {
+    for (int iteration = 0; iteration < iterations; ++iteration) {
         for (int row = 0; row < 1024; ++row) {
             for (int column = 0; column < 1024; ++column) {
                 int sum = 0;
@@ -90,7 +85,7 @@ int main(int argc, char *argv[]) {
                         int column_index = column - filter_column + 2;
                         if (0 <= row_index && row_index < 1024 && 0
                         <= column_index && column_index < 1024) {
-                            sum += data[row_index][column_index].o_val
+                            sum += data[row_index][column_index].original_vals
                                    * filter[filter_row][filter_column];
                         }
                     }
@@ -98,36 +93,37 @@ int main(int argc, char *argv[]) {
                 sum = sum / 16; // Scaling
                 // Saturation
                 if (sum > 16) {
-                    data[row][column].n_val = 16;
+                    data[row][column].new_vals = 16;
                 }
                 else if (sum >= -16) {
-                    data[row][column].n_val = sum;
+                    data[row][column].new_vals = sum;
                 }
                 else {
-                    data[row][column].n_val = -16;
+                    data[row][column].new_vals = -16;
                 }
             }
         }
         for (int row = 0; row < 1024; ++row) {
             for (int column = 0; column < 1024; ++column) {
-                data[row][column].o_val = data[row][column].n_val;
+                data[row][column].original_vals = data[row][column].new_vals;
             }
         }
     }
 
-    for (i = 0; i < 1024; i++) {
-        for (j = 0; j < 1024; j++) {
-            fprintf(file3, "%d ", data[i][j].n_val);
+    for (index = 0; index < 1024; index++) {
+        for (index2 = 0; index2 < 1024; index2++) {
+            fprintf(output_file, "%d ",
+                    data[index][index2].new_vals);
         }
-        fprintf(file3, "\n");
+        fprintf(output_file, "\n");
     }
 
-    for (i = 0; i < 1024; i++) {
-        free(data[i]);
+    for (index = 0; index < 1024; index++) {
+        free(data[index]);
     }
     free(data);
-    fclose(file1);
-    fclose(file2);
-    fclose(file3);
+    fclose(data_file);
+    fclose(filter_file);
+    fclose(output_file);
     return 0;
 }
