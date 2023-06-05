1cc962733c2a32120cb52fe754a3180026aea0d1
Mihir Patil <mightymihir100@gmail.com>
2023-06-05 17:32:56 +1200

    Scaling implemented.

--- convolution2.c
+++ convolution2.c
@@ -88,7 +88,7 @@ int main(int argc, char *argv[]) {
                     }
                 }
             }
-            // scaling
+            sum = sum / 16;
             // saturation
         }
     }
